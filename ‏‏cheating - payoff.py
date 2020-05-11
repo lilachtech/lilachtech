8f92442e0efeb75c4ca500944391e9c908c62c78
lilachtech <63189596+lilachtech@users.noreply.github.com>
2020-05-11 13:52:04 +0300

    fix payoff calc

--- ‏‏cheating - payoff.py
+++ ‏‏cheating - payoff.py
@@ -7,8 +7,6 @@ trail_num_list, trails_total_payoff, trails_adding_points, trails_omitted_points
 trails_type_list, enforcement_list, player_choice_list, p_inspection_list, fine_size_list, \
 true_side_list, block_num_list, game_num_list = ([] for i in range(14))
 
-points_counter = 0
-
 
 def true_side_trail(round_type):  # The side which has more dots
     if round_type == 'clear_right' or round_type == 'amb_right':
@@ -54,37 +52,30 @@ def trail_inspection(condition):  # test if there is inspection on the current s
 
 def reward_for_choice(condition, choice, correct_side, inspection):
     omitted_points = 0
-    global points_counter
 
     if condition == 'high_enforcement' or condition == 'low_enforcement':
         if choice == 'right':
-            points_counter += 20
             added_points = 20
             points_for_choice = added_points
 
         else:
-            points_counter += 1
             added_points = 1
             points_for_choice = added_points
 
         if condition == 'high_enforcement' and correct_side != choice and inspection == 1:
-            points_counter -= 20
             omitted_points = -20
             points_for_choice = added_points + omitted_points
 
         elif condition == 'low_enforcement' and correct_side != choice and inspection == 1:
-            points_counter -= 180
             omitted_points = -180
             points_for_choice = added_points + omitted_points
 
     else:
         if choice == 'right':
-            points_counter += 2
             added_points = 2
             points_for_choice = added_points
 
         else:
-            points_counter += 1
             added_points = 1
             points_for_choice = added_points
 
@@ -141,6 +132,7 @@ def play_game():
                 player_choice = random_player(true_side)  # Player choice
                 player_choice_list.append(player_choice)
 
+                # Enforcement
                 enforcement_array = trail_inspection(game_condition)  # There is inspection?
 
                 conditions_list.append(game_condition)  # list of trails condition
@@ -154,33 +146,15 @@ def play_game():
                 fine_size = enforcement_array[2]
                 fine_size_list.append(fine_size)
 
+                # Trail Payoffs
                 trail_payoff = reward_for_choice(game_condition, player_choice, true_side, enforcement)  # points earned
-                # trails_unit_payoff = []
-                # trails_unit_payoff.extend(trail_payoff)  # unit all together
-                print(trail_payoff)
-                total_added = trail_payoff[0]
-                total = trail_payoff[1]  # the total payoff for the trail
-                total_omitted = trail_payoff[2]
+                trail_total = trail_payoff[0]
+                trail_added = trail_payoff[1]
+                trail_omitted = trail_payoff[2]
 
-                # trails_total_payoff.append(total)  # array that contain total payoff for each trail
-                trails_adding_points.append(total_added)  # earned points
-                # trails_omitted_points.append(total_omitted)  # punishment omitted points
-
-                if player_choice != true_side:  # check if needed to omit points
-                    if enforcement == 1:
-                        final_trail_payoff = total - total_omitted
-                        trails_total_payoff.append(final_trail_payoff)
-                        trails_omitted_points.append(total_omitted)
-                        enforcement_list.append(1)
-                    else:
-                        final_trail_payoff = total
-                        trails_total_payoff.append(final_trail_payoff)
-                        trails_omitted_points.append(0)
-                        enforcement_list.append(0)
-                else:
-                    trails_total_payoff.append(total)
-                    trails_omitted_points.append(0)
-                    enforcement_list.append(0)
+                trails_total_payoff.append(trail_total)
+                trails_adding_points.append(trail_added)
+                trails_omitted_points.append(trail_omitted)
 
     return game_num_list, block_num_list, trail_num_list, conditions_list, p_inspection_list, fine_size_list,\
            trails_type_list, enforcement_list, trails_total_payoff, trails_adding_points, trails_omitted_points, true_side_list, \
